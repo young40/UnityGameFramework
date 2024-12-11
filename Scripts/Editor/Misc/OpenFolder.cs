f1d9ff351d63ec1a9e28da6e8409a3622d022207
young40 <young40@gmail.com>
2024-12-11 10:10:36 +0800

    open folder support linux

--- Scripts/Editor/Misc/OpenFolder.cs
+++ Scripts/Editor/Misc/OpenFolder.cs
@@ -82,6 +82,10 @@ namespace UnityGameFramework.Editor
                 case RuntimePlatform.OSXEditor:
                     Process.Start("open", folder);
                     break;
+                
+                case RuntimePlatform.LinuxEditor:
+                    Process.Start("open", folder);
+                    break;
 
                 default:
                     throw new GameFrameworkException(Utility.Text.Format("Not support open folder on '{0}' platform.", Application.platform));
